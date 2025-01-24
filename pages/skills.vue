1f241a1dd94a1a1900fea13e677e0de27cbba91e
Chad Feierstein <103460020+FChad@users.noreply.github.com>
2025-01-24 15:02:08 +0100

    started with skills page

--- pages/skills.vue
+++ pages/skills.vue
@@ -3,7 +3,15 @@
 </script>
 
 <template>
-    <h1 class="text-4xl font-extrabold leading-none tracking-tight text-gray-900">Skills</h1>
+    <div class="h-full w-full z-10 overflow-y-auto">
+        <div class="max-w-7xl h-full w-full mx-auto px-1 py-4 font-semibold flex flex-col gap-4">
+            <div class="flex flex-col lg:flex-row justify-between items-center gap-4">
+                <div class="flex items-center gap-4">
+                    <div class="text-xl font-bold">Skills</div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="css" scoped></style>
