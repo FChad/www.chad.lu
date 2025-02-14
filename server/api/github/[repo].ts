5bcc87fc01c35e9db3f622b710bd2fd25bacb814
Chad Feierstein <103460020+FChad@users.noreply.github.com>
2025-02-14 08:48:58 +0100

    fixed timezone in api request

--- server/api/github/[repo].ts
+++ server/api/github/[repo].ts
@@ -61,7 +61,8 @@ export default defineEventHandler(async (event) => {
                         month: '2-digit',
                         day: '2-digit',
                         hour: '2-digit',
-                        minute: '2-digit'
+                        minute: '2-digit',
+                        timeZone: 'Europe/Berlin'
                     }).format(date)
                 }
 
